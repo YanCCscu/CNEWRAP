6e0df838fcf0af005f3651ff772546bdc5fb0351
liruihanguo <86306810+liruihanguo@users.noreply.github.com>
2026-03-20 10:21:27 +0800

    Add files via upload

---
+++ scripts/merge_two_species.sh
@@ -0,0 +1,12 @@
+grep "^Tbai" ../Tbai_inputs/bed_fasta/*maf/*.gff |sed 's/.\+://' >Tbai_refer_Tbai_cnes.gff
+grep "^Tbai" ../Tele_inputs/bed_fasta/*maf/*.gff |sed 's/.\+://' >Tele_refer_Tbai_cnes.gff
+
+awk 'BEGIN{OFS="\t"} {if($2 > $3){temp=$2; $2=$3; $3=temp} print $0}' Tbai_refer_Tbai_cnes.gff > Tbai_refer_Tbai_fixed.gff
+awk 'BEGIN{OFS="\t"} {if($2 > $3){temp=$2; $2=$3; $3=temp} print $0}' Tele_refer_Tbai_cnes.gff > Tele_refer_Tbai_fixed.gff
+
+bedtools intersect -a Tbai_refer_Tbai_fixed.gff -b Tele_refer_Tbai_fixed.gff -g Tbai_genome.size -wo >CompareCNEs_Tbai.table
+
+bedtools subtract -a Tbai_refer_Tbai_fixed.gff -b Tele_refer_Tbai_fixed.gff -A >Tbai-Tele_Tbai_diffr.gff
+bedtools subtract -a Tele_refer_Tbai_fixed.gff -b Tbai_refer_Tbai_fixed.gff -A >Tele-Tbai_Tbai_diffr.gff
+
+cat CompareCNEs_Tbai.table Tbai-Tele_Tbai_diffr.gff Tele-Tbai_Tbai_diffr.gff >merged_TbaiCNEs.gff
